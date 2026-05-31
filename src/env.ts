dae033d52b3d966cc73052d9e00824e946686920
luiz <luiz@Powrss-MacBook-Pro.local>
2026-05-31 13:56:44 -0300

    base for r2 connection via s3 api

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,12 @@ import { z } from "zod"
 const envSchema = z.object({
 	DATABASE_URL: z.url(),
 	NODE_ENV: z.enum(["development", "test", "production"]),
-	PORT: z.string().transform(value => parseInt(value, 10)),
+	PORT: z.string().transform((value) => parseInt(value, 10)),
+	CLOUDFLARE_ACCOUNT_ID: z.string(),
+	CLOUDFLARE_ACCESS_KEY_ID: z.string(),
+	CLOUDFLARE_SECRET_ACCESS_KEY: z.string(),
+	CLOUDFLARE_BUCKET: z.string(),
+	CLOUDFLARE_PUBLIC_URL: z.url(),
 })
 
 export const env = envSchema.parse(process.env)
